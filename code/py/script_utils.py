5e9cfd942a36b65cdb953b80db5ffae3ab216407
Martin Szyska <martin.szyska37@gmail.com>
2022-11-16 12:12:41 +0100

    add get_path function

--- code/py/script_utils.py
+++ code/py/script_utils.py
@@ -49,23 +49,29 @@ def load_config(config_file="", *, config_path="", **kwargs):
     directly passed arguments overwrite config
     '''
 
+    # build the config_file path from config_file and config_path arguments
     if config_path and not config_file.startswith("/"):
         config_file = os.path.join(config_path, config_file)
-    if not os.path.splitext(config_file) in [".yml", "yaml"]:
+    if not os.path.splitext(config_file)[-1] in [".yml", "yaml"]:
         config_file = config_file + ".yml"
+    # savely load the config file into config dict
     try:
         config = load_config_file(config_file)
     except:
         show_output(f"config file {config_file} could not be loaded", color="warning")
         return {}
 
+    # build base_path relative to HOME path
     path_config = config['paths']
     if not path_config['base_path'].startswith("/"):
         path_config['base_path'] = os.path.join(os.environ['HOME'], path_config['base_path'])
+    
+    # build other paths relative to base_path
     for path in ['data', 'output', 'info', 'img', 'tables']:
         key = f"{path}_path"
-        if not path_config[key].startswith("/"):
-            path_config[key] = os.path.join(path_config['base_path'], path_config[key])
+        if key in path_config:
+            if not path_config[key].startswith("/"):
+                path_config[key] = os.path.join(path_config['base_path'], path_config[key])
 
     # load in the kwargs to overwrite config
     config.update(kwargs)
@@ -73,12 +79,34 @@ def load_config(config_file="", *, config_path="", **kwargs):
     # build the output paths
     pc = config['paths']
     for folder in ['output_path', 'img_path', 'tables_path']:
-        if not os.path.isdir(pc[folder]):
-            show_output(f"Creating folder {pc[folder].replace(pc['base_folder'], '')} in base folder")
-            os.makedirs(pc[folder])
+        if folder in pc:
+            if not os.path.isdir(pc[folder]):
+                show_output(f"Creating folder {pc[folder].replace(pc['base_path'], '')} in base folder")
+                os.makedirs(pc[folder])
 
     if (code_base := pc['code_core']):
         sys.path.append(code_base)
         show_output(f"Added {code_base} to python path for imports")
 
     return config
+
+
+def get_path(path, file_type="file", config={}):
+    '''
+    retrieves a path value from the given key in the config and does some checks
+    '''
+    pc = config['paths']
+    if not path in pc:
+        show_output(f"Please provide a path for the {file_type} in the configs @{path}!", color="warning")
+        return
+    if not (file_path := pc[path]):
+        show_output("Please provide a {file_type} in the configs", color="warning")
+        return
+    if not (file_path := pc[path]).startswith("/"):
+        file_path = os.path.join(os.environ['HOME'], pc[path])
+        
+    if os.path.isfile(file_path):
+        return file_path
+    else:
+        show_output(f"{file_type} {file_path} cannot be found!", color="warning")
+        return
